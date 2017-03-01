207745fca18aaaaa4da90e433a44eda03a994be4
Ivan Marović <ivanceinwow@gmail.com>
2017-03-01 22:49:58 +0100

    Initial state.

---
+++ app/src/main/java/com/ivan/vts/mapper/map/MapsActivity.java
@@ -0,0 +1,194 @@
+package com.ivan.vts.mapper.map;
+
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.internal.PopupManager;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.plus.Account;
+import com.google.gson.Gson;
+import com.ivan.vts.mapper.R;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class MapsActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, LocationListener {
+
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    LatLng latLng;
+    GoogleMap mGoogleMap;
+    SupportMapFragment mFragment;
+    Marker currLocationMarker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        try {
+            String accountMail = AccountManager.get(getApplicationContext()).getAccountsByType("com.google")[0].name;
+            Toast.makeText(getApplicationContext(), "Account email: " + accountMail, Toast.LENGTH_LONG).show();
+            if (accountMail == null || accountMail.isEmpty()) {
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Account name")
+                        .setContentText("Application requires Google account.")
+                        .setConfirmText("Exit application")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+                                sDialog.dismissWithAnimation();
+                            }
+                        })
+                        .show();
+                exitApplication();
+            }
+        } catch (SecurityException e) {
+            exitApplication();
+        }
+
+        mFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mFragment.getMapAsync(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return com.ivan.vts.mapper.settings.Settings.getInstance().helloWorld(this, item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap gMap) {
+        mGoogleMap = gMap;
+        try {
+            mGoogleMap.setMyLocationEnabled(true);
+        } catch (SecurityException e) {}
+
+        buildGoogleApiClient();
+
+        mGoogleApiClient.connect();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location mLastLocation = null;
+
+        try{
+             mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } catch (SecurityException e) {}
+
+        if (mLastLocation != null) {
+            //place marker at current position
+            //mGoogleMap.clear();
+            latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); //5 seconds
+        mLocationRequest.setFastestInterval(3000); //3 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        try{
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } catch (SecurityException e) {}
+
+        PolylineOptions rectOptions = new PolylineOptions()
+//                .color(R.color.colorMenuItems)
+                .add(new LatLng(37.35, -122.0))
+                .add(new LatLng(37.45, -122.0))  // North of the previous point, but at the same longitude
+                .add(new LatLng(37.45, -122.2))  // Same latitude, and 30km to the west
+                .add(new LatLng(37.35, -122.2))  // Same longitude, and 16km to the south
+                .add(new LatLng(37.35, -122.0)); // Closes the polyline.
+
+// Get back the mutable Polyline
+        Polyline polyline = mGoogleMap.addPolyline(rectOptions);
+
+
+//        Uri gmmIntentUri = Uri.parse("google.navigation:q=Brace+Radica+80+Subotica");
+//        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//        mapIntent.setPackage("com.google.android.apps.maps");
+
+//        startActivity(mapIntent);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        //place marker at current position
+        //mGoogleMap.clear();
+        if (currLocationMarker != null) {
+            currLocationMarker.remove();
+        }
+        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        //zoom to current position:
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng).zoom(14).build();
+
+        mGoogleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+    }
+
+    private void exitApplication() {
+        this.finish();
+        System.exit(0);
+    }
+}
---
+++ app/src/main/java/com/ivan/vts/mapper/settings/Settings.java
@@ -0,0 +1,40 @@
+package com.ivan.vts.mapper.settings;
+
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ivan.vts.mapper.R;
+
+/**
+ *
+ * @author Ivan Marovic
+ * @version 1.0
+ * 18/2/2017.
+ */
+
+public class Settings {
+
+    MenuItem menuItem;
+
+    private static Settings settings = new Settings();
+
+    private Settings() {
+    }
+
+    public static Settings getInstance() {
+        return settings;
+    }
+
+    public boolean helloWorld(Context context, MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if (id == R.id.settings) {
+            Toast.makeText(context, "hello world", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return false;
+    }
+
+}
