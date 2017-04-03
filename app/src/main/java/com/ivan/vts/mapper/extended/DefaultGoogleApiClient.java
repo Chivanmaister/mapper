b0c35a69e9cdc6cceb15c8b7e41374e2fc199b4f
Ivan Marović <ivanceinwow@gmail.com>
2017-04-03 21:18:01 +0200

    More refactored, created response from Google Map API in JSON

---
+++ app/src/main/java/com/ivan/vts/mapper/extended/DefaultGoogleApiClient.java
@@ -0,0 +1,93 @@
+package com.ivan.vts.mapper.extended;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.ivan.vts.mapper.R;
+
+/**
+ * Created by Chiefster on 3/4/2017.
+ */
+
+public class DefaultGoogleApiClient extends DefaultAppActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected LocationRequest mLocationRequest;
+    protected GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Location mLastLocation = null;
+
+        try {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } catch (SecurityException e) {
+        }
+
+        if (mLastLocation != null) {
+            //place marker at current position
+            //mGoogleMap.clear();
+            latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        }
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); //5 seconds
+        mLocationRequest.setFastestInterval(3000); //3 seconds
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
+
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } catch (SecurityException e) {
+        }
+
+        PolylineOptions rectOptions = new PolylineOptions()
+//                .color(R.color.colorMenuItems)
+                .add(new LatLng(37.35, -122.0))
+                .add(new LatLng(37.45, -122.0))  // North of the previous point, but at the same longitude
+                .add(new LatLng(37.45, -122.2))  // Same latitude, and 30km to the west
+                .add(new LatLng(37.35, -122.2))  // Same longitude, and 16km to the south
+                .add(new LatLng(37.35, -122.0)); // Closes the polyline.
+
+        // Get back the mutable Polyline
+        Polyline polyline = mGoogleMap.addPolyline(rectOptions);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        Location finalMLastLocation = mLastLocation;
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+            }
+
+            @Override
+            public void onError(Status status) {
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
--- app/src/main/java/com/ivan/vts/mapper/map/MapsActivity.java
+++ app/src/main/java/com/ivan/vts/mapper/map/MapsActivity.java
@@ -41,6 +41,7 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.ivan.vts.mapper.R;
 import com.ivan.vts.mapper.extended.DefaultAppActivity;
+import com.ivan.vts.mapper.extended.DefaultGoogleApiClient;
 import com.ivan.vts.mapper.settings.helper.Setting;
 
 import java.io.BufferedInputStream;
@@ -48,15 +49,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
-public class MapsActivity extends DefaultAppActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+public class MapsActivity extends DefaultGoogleApiClient implements OnMapReadyCallback {
 
-    protected LocationRequest mLocationRequest;
-    protected GoogleApiClient mGoogleApiClient;
+
+
     protected SupportMapFragment mFragment;
 //    protected static final String url = "https://maps.googleapis.com/maps/api/directions/json?";
 
@@ -66,9 +70,9 @@ public class MapsActivity extends DefaultAppActivity implements GoogleApiClient.
         setContentView(R.layout.activity_maps);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        //Pop up for invalid account
         try {
-            String accountMail = AccountManager.get(getApplicationContext()).getAccountsByType("com.google")[0].name;
-            if (accountMail == null || accountMail.isEmpty()) {
+            if (Arrays.asList(AccountManager.get(getApplicationContext()).getAccountsByType("com.google")).isEmpty()) {
                 new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
                         .setTitleText("Account name")
                         .setContentText("Application requires Google account.")
@@ -83,6 +87,22 @@ public class MapsActivity extends DefaultAppActivity implements GoogleApiClient.
 
         mFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mFragment.getMapAsync(this);
+
+        //Get json from Google Map Api
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=Subotica&destination=Belgrade";
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(request);
     }
 
     @Override
@@ -117,69 +137,6 @@ public class MapsActivity extends DefaultAppActivity implements GoogleApiClient.
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Location mLastLocation = null;
-
-        try {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        } catch (SecurityException e) {
-        }
-
-        if (mLastLocation != null) {
-            //place marker at current position
-            //mGoogleMap.clear();
-            latLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-        }
-
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(5000); //5 seconds
-        mLocationRequest.setFastestInterval(3000); //3 seconds
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
-
-        try {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        } catch (SecurityException e) {
-        }
-
-        PolylineOptions rectOptions = new PolylineOptions()
-//                .color(R.color.colorMenuItems)
-                .add(new LatLng(37.35, -122.0))
-                .add(new LatLng(37.45, -122.0))  // North of the previous point, but at the same longitude
-                .add(new LatLng(37.45, -122.2))  // Same latitude, and 30km to the west
-                .add(new LatLng(37.35, -122.2))  // Same longitude, and 16km to the south
-                .add(new LatLng(37.35, -122.0)); // Closes the polyline.
-
-    // Get back the mutable Polyline
-        Polyline polyline = mGoogleMap.addPolyline(rectOptions);
-
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
-                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
-
-        Location finalMLastLocation = mLastLocation;
-
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-
-            }
-
-            @Override
-            public void onError(Status status) {
-            }
-        });
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
     }
 
     private void exitApplication() {
